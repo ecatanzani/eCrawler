43f63772c6ebcde760daf441b433feda72a70602
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2019-12-10 18:24:44 +0100

    Added option to open a single file or a list of files

--- spotter.py
+++ spotter.py
@@ -12,7 +12,8 @@ def main(args=None):
     ### Parsing options
     parser = ArgumentParser(usage="Usage: %(prog)s [options]", description="electron skimmer")
 
-    parser.add_argument("-i","--input",dest='input', help='the input files to be processed ')
+    parser.add_argument("-i","--input",dest='input', help='single input file to process ')
+    parser.add_argument("-l","--list",dest='list', help='list of files to process ')
     parser.add_argument("-v","--verbose", action='store_true', default=False, dest='verbose', help='run in high verbosity mode')
     #parser.add_argument("-q","--quiet", action='store_true', default=False, dest='quiet', help='suppress a lot of output, quiet mode')
     parser.add_argument("-o","--output",default="skimmed_data/", type=str, dest='output', help='name of output directory')
